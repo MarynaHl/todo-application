fa8b9e049595a594c1163590baacb921a32fd178
Maryna Hlushak <75501308+MarynaHl@users.noreply.github.com>
2025-02-12 15:39:02 +0000

    feat: add MongoDB models for User and Task

--- backend/server.js
+++ backend/server.js
@@ -13,5 +13,9 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+require("./models/User");
+require("./models/Task");
+
