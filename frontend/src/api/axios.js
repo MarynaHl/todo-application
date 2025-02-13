5cd09aee915c91ba97c16ae0b2c425083af4a455
Maryna Hlushak <75501308+MarynaHl@users.noreply.github.com>
2025-02-13 12:58:57 +0000

    Set up Axios with authentication interceptor

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,9 +2,6 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: "http://127.0.0.1:5001",
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
 
 API.interceptors.request.use((config) => {
